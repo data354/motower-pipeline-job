b5ab5e89d7b4953401f6c3dfea7b7fe67487dd1e
yasmine-datalab <yasminekouadio@hotmail.fr>
2023-03-27 18:08:50 +0000

    add ftp data ingest

--- dags/gps/custom_dags/extract_archive.py
+++ dags/gps/custom_dags/extract_archive.py
@@ -17,6 +17,9 @@ MINIO_ENDPOINT = Variable.get('minio_host')
 MINIO_ACCESS_KEY = Variable.get('minio_access_key')
 MINIO_SECRET_KEY = Variable.get('minio_secret_key')
 
+FTP_HOST = Variable.get('ftp_host')
+FTP_USER = Variable.get('ftp_user')
+FTP_PASSWORD = Variable.get('ftp_password')
 # Get BD settings
 
 #db_file = Path(__file__).parents[3] / "config/database.yaml"
@@ -50,6 +53,16 @@ def extract_a_job(**kwargs):
 
     if data.shape[0] != 0:
         save_minio(MINIO_ENDPOINT, MINIO_ACCESS_KEY, MINIO_SECRET_KEY, kwargs["bucket"], kwargs["folder"], kwargs["ingest_date"], data)
+    else:
+        raise RuntimeError(f"No data for {kwargs['ingest_date']}")
+
+def extract_a_ftp_job(**kwargs):
+    """
+    """
+
+    data = extract_ftp(FTP_HOST,FTP_USER, FTP_PASSWORD , kwargs["ingest_date"])
+    if data.shape[0] != 0:
+        save_minio(MINIO_ENDPOINT, MINIO_ACCESS_KEY, MINIO_SECRET_KEY, kwargs["bucket"], kwargs["folder"] , kwargs["ingest_date"], data)
     else:
         raise RuntimeError(f"No data for {kwargs['ingest_date']}")
 
@@ -132,9 +145,18 @@ with DAG(
                    'folder': config["tables"][6]["folder"],
                    'ingest_date': INGEST_DATE},
         dag=dag,
+    ),
+    ingest_ftp = PythonOperator(
+        task_id='ingest_ftp',
+        provide_context=True,
+        python_callable=extract_a_ftp_job,
+        op_kwargs={'bucket': config["tables"][7]["bucket"],
+                   'folder': config["tables"][7]["folder"],
+                   'ingest_date': INGEST_DATE},
+        dag=dag,
     )
 
-    [ingest_hdrp, ingest_ts2g, ingest_ts3g, ingest_cd2g, ingest_cd3g, ingest_indis ]
+    [ingest_hdrp, ingest_ts2g, ingest_ts3g, ingest_cd2g, ingest_cd3g, ingest_indis, ingest_ftp ]
 
 if __name__ == "__main__":
     from airflow.utils.state import State
--- dags/gps/custom_dags/extract_pg.py
+++ dags/gps/custom_dags/extract_pg.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from datetime import datetime
 import json
 from airflow import DAG
-from gps.common.extract import extract_pg
+from gps.common.extract import extract_pg, extract_ftp
 from gps.common.rwminio import save_minio
 from airflow.operators.python import PythonOperator
 from airflow.models import Variable
@@ -17,6 +17,10 @@ PG_PASSWORD = Variable.get('pg_password')
 MINIO_ENDPOINT = Variable.get('minio_host')
 MINIO_ACCESS_KEY = Variable.get('minio_access_key')
 MINIO_SECRET_KEY = Variable.get('minio_secret_key')
+
+FTP_HOST = Variable.get('ftp_host')
+FTP_USER = Variable.get('ftp_user')
+FTP_PASSWORD = Variable.get('ftp_password')
 
 INGEST_DATE = "{{ macros.ds_add(ds, -1) }}"
 
@@ -43,6 +47,15 @@ def extract_job(**kwargs):
     else:
         raise RuntimeError(f"No data for {kwargs['ingest_date']}")
 
+def extract_ftp_job(**kwargs):
+    """
+    """
+
+    data = extract_ftp(FTP_HOST,FTP_USER, FTP_PASSWORD , kwargs["ingest_date"])
+    if data.shape[0] != 0:
+        save_minio(MINIO_ENDPOINT, MINIO_ACCESS_KEY, MINIO_SECRET_KEY, kwargs["bucket"], kwargs["folder"] , kwargs["ingest_date"], data)
+    else:
+        raise RuntimeError(f"No data for {kwargs['ingest_date']}")
 
 with DAG(
     'extract_pg',
@@ -121,9 +134,18 @@ with DAG(
                    'folder': config["tables"][6]["folder"],
                    'ingest_date': INGEST_DATE},
         dag=dag,
+    ),
+    ingest_ftp = PythonOperator(
+        task_id='ingest_ftp',
+        provide_context=True,
+        python_callable=extract_ftp_job,
+        op_kwargs={'bucket': config["tables"][7]["bucket"],
+                   'folder': config["tables"][7]["folder"],
+                   'ingest_date': INGEST_DATE},
+        dag=dag,
     )
 
-    [ingest_hdrp, ingest_ts2g, ingest_ts3g, ingest_cd2g, ingest_cd3g, ingest_indis]
+    [ingest_hdrp, ingest_ts2g, ingest_ts3g, ingest_cd2g, ingest_cd3g, ingest_indis, ingest_ftp]
 
 if __name__ == "__main__":
     from airflow.utils.state import State
