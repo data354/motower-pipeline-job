d2fc441be18964fa3e5d65e1ab671d67a24a550a
yasmine-datalab <yasminekouadio@hotmail.fr>
2023-03-30 01:51:24 +0000

    add verif on ingest and clean bdd sites

--- dags/gps/__init__.py
+++ dags/gps/__init__.py
@@ -3,7 +3,7 @@ import json
 
 
 
-config_file = Path(__file__).parents[3] / "config/configs.json"
+config_file = Path(__file__).parents[2] / "config/configs.json"
 if config_file.exists():
     with config_file.open("r",) as f:
         CONFIG = json.load(f)
