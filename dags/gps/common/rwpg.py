ff1d25cc23ad880bd09d0d20bdfe8bc5a04f4668
yasmine-datalab <yasminekouadio@hotmail.fr>
2023-04-19 19:15:33 +0000

    push to pg

--- dags/gps/common/rwpg.py
+++ dags/gps/common/rwpg.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-def write_pg(host: str, database:str, user: str, password: str, data, table: str = None, port:str=5432):
+def write_pg(host: str, database:str, user: str, password: str, data, table: str = None, port:str="5432"):
     """"
      write data in pg
     """
