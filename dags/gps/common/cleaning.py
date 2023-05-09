c20eddf26bef6521b34f3947b0638529cc25b6a0
Yasmine Stéphanie KOUADIO <yasminekouadio@hotmail.fr>
2023-05-09 17:20:14 +0000

    fix breakout ihs

--- dags/gps/common/cleaning.py
+++ dags/gps/common/cleaning.py
@@ -234,11 +234,11 @@ def cleaning_ihs(endpoint:str, accesskey:str, secretkey:str,  date: str)-> None:
                 data_final["discount"] = 0
                 data_final["volume discount"] = 0    
                 
-                data_final["o&m"] = ratio["o&m"] * data_final["month_total"]
-                data_final["energy"] = ratio["energy"] * data_final["month_total"]
-                data_final["infra"] = ratio["infra"] * data_final["month_total"]
-                data_final["maintenance passive préventive"] = ratio["maintenance passive préventive"] * data_final["month_total"]
-                data_final["gardes de sécurité"] = ratio["gardes de sécurité"] * data_final["month_total"]
+                data_final["o&m"] = ratio["o&m"].values * data_final["month_total"]
+                data_final["energy"] = ratio["energy"].values * data_final["month_total"]
+                data_final["infra"] = ratio["infra"].values * data_final["month_total"]
+                data_final["maintenance passive préventive"] = ratio["maintenance passive préventive"].values * data_final["month_total"]
+                data_final["gardes de sécurité"] = ratio["gardes de sécurité"].values * data_final["month_total"]
             else:
                 data_final["discount"] = 0
                 data_final["volume discount"] = 0  
