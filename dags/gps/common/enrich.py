f5bff531f38854589fea1d2d6cf9decd0209eb54
Yasmine Stéphanie KOUADIO <yasminekouadio@hotmail.fr>
2023-06-07 13:19:36 +0000

    change enrich

--- dags/gps/common/enrich.py
+++ dags/gps/common/enrich.py
@@ -260,7 +260,7 @@ def oneforall(client, endpoint:str, accesskey:str, secretkey:str,  date: str, st
     oneforall["INTERCO"] = oneforall["CA_VOIX"]*interco
     oneforall["IMPOT"] = oneforall["CA_TOTAL"]*impot
     oneforall["FRAIS_DIST"] = oneforall["CA_TOTAL"]*frais_dist
-    oneforall["OPEX TOTAL"] = oneforall['OPEX'] + oneforall["INTERCO"] + oneforall["IMPOT"] + oneforall["FRAIS_DIST"]
+    oneforall["OPEX_TOTAL"] = oneforall['OPEX'] + oneforall["INTERCO"] + oneforall["IMPOT"] + oneforall["FRAIS_DIST"]
 
     oneforall["EBITDA"] = oneforall["CA_TOTAL"] - oneforall["OPEX_TOTAL"]
 
