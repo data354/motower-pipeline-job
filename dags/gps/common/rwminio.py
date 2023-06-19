487a06ac69ae7044df6895037a5a805296199598
Yasmine Stéphanie KOUADIO <yasminekouadio@hotmail.fr>
2023-06-19 16:15:08 +0000

    rewrite and clean code of rwminio

--- dags/gps/common/rwminio.py
+++ dags/gps/common/rwminio.py
@@ -1,30 +1,28 @@
 """ UTILS FUNCTION FOR MINIO"""
-
+from typing import List
 import logging
 from io import BytesIO
 from pathlib import Path
 
-
-
-def save_minio(client, bucket: str, folder: str , date: str, data) -> None:
+def save_minio(client, bucket: str, folder: str, date: str, data) -> None:
     """
-        save dataframe in minio
-        Args:
-            table [str]
-            date [str]
-            df [pd.DataFrame]
-        Return
-            None
+    save dataframe in minio
+    Args:
+        client: Minio client object
+        bucket: Name of the bucket to save the file in
+        folder: Folder path within the bucket to save the file in
+        date: Date string in the format "YYYY-MM-DD"
+        data: Pandas DataFrame object to save
     """
-    
     logging.info("start to save data")
     if not client.bucket_exists(bucket):
         client.make_bucket(bucket)
-    csv_bytes = data.to_csv().encode('utf-8')
+    csv_bytes = data.to_csv(index=False).encode('utf-8')
     csv_buffer = BytesIO(csv_bytes)
     if folder is not None:
         client.put_object(bucket,
-                       f"{folder}/{date.split('-')[0]}/{date.split('-')[1]}/{date.split('-')[2]}.csv",
+                       f"""{folder}/{date.split('-')[0]}/{date.split('-')[1]}/
+                       {date.split('-')[2]}.csv""",
                         data=csv_buffer,
                         length=len(csv_bytes),
                         content_type='application/csv')
@@ -44,7 +42,6 @@ def save_file_minio(client, bucket:str, file:str):
     save file into minio
     
     """
-    
     logging.info("start to save file")
     if not client.bucket_exists(bucket):
         client.make_bucket(bucket)
@@ -52,35 +49,41 @@ def save_file_minio(client, bucket:str, file:str):
     client.fput_object(bucket, file, path)
 
 
-def getfilename(client,bucket:str, prefix:str = None,
+def get_last_filename(client,bucket:str, prefix:str = None,
                 recursive:bool=True ):
     """
         get filename
     """
-    
     objets = list(client.list_objects(bucket_name=bucket, prefix=prefix, recursive=recursive))
-    if len(objets) == 0:
+    if not objets:
         raise RuntimeError(f"file {prefix} don't exists")
-    
     last_date = max([obj.last_modified for obj in objets])
-    
-    filename = [obj.object_name for obj in objets if obj.last_modified == last_date][0]
-    if (not filename.lower().endswith(".xlsx")) and (not filename.lower().endswith(".xls")) and (not filename.lower().endswith(".csv")):
+    filename = [obj.object_name for obj in objets if obj.last_modified == last_date
+                and obj.object_name.endswith([".xlsx", ".xls", "csv"])]
+    if not filename:
         raise ValueError("file with good extension not found")
-    return filename
+    return filename[0]
 
 
-def getfilesnames(client,bucket:str, prefix:str = None,
-                recursive:bool=True):
-    
+
+def get_files(client, bucket: str, prefix: str = None, extensions: List[str] = None) -> List[str]:
     """
-        get files names
+    Get a list of names of all files in the bucket that match the given prefix and extensions.
+    Args:
+        client: Minio client object
+        bucket: Name of the bucket to search in
+        prefix: Prefix to filter the files by
+        extensions: List of extensions to filter the files by
+    Returns:
+        List of names of all files that match the given criteria
     """
-    
-    objets = list(client.list_objects(bucket_name=bucket, prefix=prefix, recursive=recursive))
-    if len(objets) == 0:
-        raise RuntimeError(f"file {prefix} don't exists")
-        
-    filenames = [obj.object_name for obj in objets if obj.object_name.lower().endswith(".xls") or obj.object_name.endswith(".xlsx") or obj.object_name.endswith(".csv")  ]
-    return filenames
-
+    objects = list(client.list_objects(bucket_name=bucket, prefix=prefix, recursive=True))
+    if not objects:
+        raise RuntimeError(f"No files found with prefix {prefix}")
+    if extensions is None:
+        extensions = []
+    files = [obj.object_name for obj in objects
+            if obj.object_name.lower().endswith(tuple(extensions))]
+    if not files:
+        raise ValueError(f"No files found with extensions {extensions}")
+    return files
