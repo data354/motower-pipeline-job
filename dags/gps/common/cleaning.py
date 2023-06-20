77356539f1e0343635f7b697245fd46c6b9b4117
Yasmine Stéphanie KOUADIO <yasminekouadio@hotmail.fr>
2023-06-20 18:16:04 +0000

    rewrite and cleaning ihs cleaning code

--- dags/gps/common/cleaning.py
+++ dags/gps/common/cleaning.py
@@ -106,122 +106,82 @@ def cleaning_ihs(client, endpoint:str, accesskey:str, secretkey:str,  date: str)
     """
      clean or enrich  ihs
     """
-    if datetime.strptime(date, CONFIG["date_format"]) >= datetime(2022,7,6):
-        if date.split("-")[1] in ["01","04","07","10"]:
-            
-            
-            objet = [d for d in CONFIG["tables"] if d["name"] == "OPEX_IHS"][0]
-            
-            if not client.bucket_exists(objet["bucket"]):
-                raise OSError(f"bucket {objet['bucket']} don\'t exits")
-
-            logging.info("get filename")
-            filename = get_last_filename(client, objet["bucket"], prefix = f"{objet['folder']}/{objet['folder']}_{date_parts[0]}{date_parts[1]}")
-            logging.info("read file %s",filename)
-
-            excel = pd.ExcelFile(f"s3://{objet['bucket']}/{filename}",
-                                storage_options={
-                                "key": accesskey,
-                                "secret": secretkey,
-                                "client_kwargs": {"endpoint_url": f"http://{endpoint}"}
-                                    } )
-            sheet_f = []
-            for sheet in objet["sheets"]:
-                sheet_f.extend([s for s in excel.sheet_names if s.find(sheet)!=-1])
-                data = pd.DataFrame()
+    date_parts = date.split("-")
+    if date_parts[1] in ["01","04","07","10"]:
+        objet = next((d for d in CONFIG["tables"] if d["name"] == "OPEX_IHS"), None)
+        if objet is None:
+            raise ValueError("Table OPEX_ESCO not found in CONFIG.")
+        if not client.bucket_exists(objet["bucket"]):
+            raise OSError(f"Bucket {objet['bucket']} does not exist.")
+        logging.info("get filename")
+        prefix = f"{objet['folder']}/{objet['folder']}_{date_parts[0]}{date_parts[1]}"
+        filename = get_latest_file(client, objet["bucket"], prefix=prefix)
+        logging.info("read file %s",filename)
+        excel = pd.read_excel(f"s3://{objet['bucket']}/{filename}",
+                              sheet_name=None,
+                              storage_options={
+                                  "key": accesskey,
+                                  "secret": secretkey,
+                                  "client_kwargs": {"endpoint_url": f"http://{endpoint}"}
+                              })
+        data = pd.DataFrame()
+        for sheet in objet["sheets"]:
+            matching_sheets = [s for s in excel.keys() if s.find(sheet) != -1]
+            for sh in matching_sheets:
                 logging.info("read %s", filename)
-                for sh in sheet_f:
-                    header = 14 if sh.find("OCI-COLOC") != -1 else 15
-                    try:
-                        
-                        df_ = pd.read_excel(f"s3://{objet['bucket']}/{filename}",
-                                        header=header , sheet_name=sh,
-                            storage_options={
+                header = 14 if sh.find("OCI-COLOC") != -1 else 15
+                df_ = pd.read_excel(excel[sh], header=header)
+                df_.columns = df_.columns.str.lower()
+                df_.columns = [unidecode(col) for col in df_.columns]
+                is_bpci_22 = sh.find("OCI-MLL BPCI 22") == -1
+                columns_to_check = ['site id ihs', 'site name', 'category', 'trimestre ht'] if is_bpci_22 else objet["columns"]
+                missing_columns = set(columns_to_check) - (set(df_.columns))
+                if missing_columns:
+                    raise ValueError(f"missing columns {', '.join(missing_columns)} in sheet {sh} of file {filename}")
+                df_ = df_.loc[:, ['site id ihs', 'site name', 'category', 'trimestre ht']] if is_bpci_22 else df_.loc[:, objet['columns']]
+                df_["month_total"] = df_['trimestre ht'] / 3 if is_bpci_22 else df_['trimestre 1 - ht'] / 3
+                data = pd.concat([data, df_])
+        logging.info("clean columns")
+        cols_to_trim = ['site id ihs']
+        data[cols_to_trim] = data[cols_to_trim].astype("str").apply(lambda x: x.str.strip())
+        data["mois"] = date_parts[0]+"-"+date_parts[1]
+        data = data.drop_duplicates(["site id ihs", "mois"], keep="first")
+        data.loc[data["trimestre ht"].isna(),"trimestre ht"] = data.loc[data["trimestre 1 - ht"].notna(), "trimestre 1 - ht"]
+        data = data.dropna(subset=['site id ihs', "trimestre ht"])
+        data_final = pd.concat([data] + [deepcopy(data).assign(mois=date_parts[0]+"-"+str(int(date_parts[1])+i).zfill(2)) for i in range(1, 3)])
+        data_final = data_final.reset_index(drop=True)
+        logging.info("Add breakout data")
+        #download esco to make ratio
+        esco_objet =  next((d for d in CONFIG["tables"] if d["name"] == "OPEX_ESCO"), None)
+        prefix = f"{objet['folder']}/{objet['folder']}_{date_parts[0]}{date_parts[1]}"
+        filename = get_latest_file(client, objet["bucket"], prefix=prefix)
+        try:
+            logging.info("read %s", filename)
+            esco = pd.read_csv(f"s3://{esco_objet['bucket']}/{filename}",
+                                 storage_options={
                                 "key": accesskey,
                                 "secret": secretkey,
-                                "client_kwargs": {"endpoint_url": f"http://{endpoint}"}
-                                        }
-                                )
-                        df_.columns = df_.columns.str.lower()
-                        df_.columns = [unidecode(col) for col in df_.columns]
-                        columns_to_check = ['site id ihs', 'site name', 'category', 'trimestre ht'] if sh.find("OCI-MLL BPCI 22") == -1 else objet["columns"]
-                        missing_columns = set(columns_to_check).difference(set(df_.columns))
-                        if len(missing_columns):
-                            raise ValueError(f"missing columns {', '.join(missing_columns)} in sheet {sh} of file {filename}")
-                        
-                        df_ = df_.loc[:, ['site id ihs', 'site name', 'category', 'trimestre ht']] if sh.find("OCI-MLL BPCI 22") == -1 else df_.loc[:, objet['columns']]
-                        df_["month_total"] = df_['trimestre ht'] / 3 if sh.find("OCI-MLL BPCI 22") == -1 else df_['trimestre 1 - ht'] / 3
-                        data = pd.concat([data, df_])
-                    except Exception as error:
-                        raise OSError(f"{filename} don't exists in bucket") from error
-
-
-            logging.info("clean columns")
-
-            cols_to_trim = ['site id ihs']
-            data[cols_to_trim] = data[cols_to_trim].apply(lambda x: x.astype("str"))
-            data[cols_to_trim] = data[cols_to_trim].apply(lambda x: x.str.strip())
-            data["mois"] = date.split("-")[0]+"-"+date.split("-")[1]
-            data = data.loc[~ data['site id ihs'].isnull(),:]
-            data = data.drop_duplicates(["site id ihs", "mois"], keep="first")
-            
-            data.loc[data["trimestre ht"].isna(),"trimestre ht"] = data.loc[data["trimestre 1 - ht"].notna(), "trimestre 1 - ht"]
-            data = data.loc[~ data["trimestre ht"].isnull(),:]
-            data1 = deepcopy(data)
-            data1["mois"] = date.split("-")[0]+"-"+str(int(date.split("-")[1])+1).zfill(2)
-            data2 = deepcopy(data)
-            data2["mois"] = date.split("-")[0]+"-"+str(int(date.split("-")[1])+2).zfill(2)
-            data_final = pd.concat([data, data1, data2])
-            data_final = data_final.reset_index(drop=True)
-
-            if datetime.strptime(date, CONFIG["date_format"]) >= datetime(2022,10,6):
-                logging.info("Add breakout data")
-
-                #download esco to make ratio
-                esco_objet = [d for d in CONFIG["tables"] if d["name"] == "OPEX_ESCO"][0]
-                filename = get_last_filename(client, esco_objet["bucket"], prefix = f"{esco_objet['folder']}-cleaned/{date.split('-')[0]}/{date.split('-')[1]}")
-                try:
-                    logging.info("read %s", filename)
-                    esco = pd.read_csv(f"s3://{esco_objet['bucket']}/{filename}",
-                                    
-                                    storage_options={
-                                    "key": accesskey,
-                                    "secret": secretkey,
-                    "client_kwargs": {"endpoint_url": f"http://{endpoint}"}
-                    }
-                        )
-                except Exception as error:
-                    raise OSError(f"{filename} don't exists in bucket") from error
-                
-                
-                esco.loc[esco["discount"].isnull(), "discount"] = 0
-                esco.loc[esco["volume discount"].isnull(), "volume discount" ]= 0
-                esco["opex_without_discount"] = esco["total redevances ht"] + esco["discount"] + esco["volume discount"]
-                esco = esco.groupby("mois").sum()
-                
-                esco = esco.loc[:,["o&m", 'energy',	"infra"	, "maintenance passive préventive", "gardes de sécurité","discount", "volume discount", "opex_without_discount"]]
-                ratio = esco.loc[: ,["o&m", 'energy',	"infra"	, "maintenance passive préventive", "gardes de sécurité","discount"]]/esco["opex_without_discount"].values[0]
-                #ratio["volume discount"] = esco["volume discount"].divide(esco["opex_without_discount"].values[0])
-                
-
-                data_final["discount"] = 0
-                data_final["volume discount"] = 0    
-                
-                data_final["o&m"] = ratio["o&m"].values * data_final["month_total"]
-                data_final["energy"] = ratio["energy"].values * data_final["month_total"]
-                data_final["infra"] = ratio["infra"].values * data_final["month_total"]
-                data_final["maintenance passive préventive"] = ratio["maintenance passive préventive"].values * data_final["month_total"]
-                data_final["gardes de sécurité"] = ratio["gardes de sécurité"].values * data_final["month_total"]
-            else:
-                data_final["discount"] = 0
-                data_final["volume discount"] = 0  
-                data_final["o&m"] = 0
-                data_final["energy"] = 0  
-                data_final["infra"] = 0
-                data_final["maintenance passive préventive"] = 0
-                data_final["gardes de sécurité"] = 0
-            logging.info("save to minio")
-            save_minio(client, objet["bucket"], f'{objet["folder"]}-cleaned', date, data_final)
+                "client_kwargs": {"endpoint_url": f"http://{endpoint}"}
+                }
+                    )
+        except Exception as error:
+            raise OSError(f"{filename} don't exists in bucket") from error
+        esco.loc[esco["discount"].isnull(), "discount"] = 0
+        esco.loc[esco["volume discount"].isnull(), "volume discount" ]= 0
+        esco["opex_without_discount"] = esco["total redevances ht"] + esco["discount"] + esco["volume discount"]
+        esco = esco.groupby("mois").sum()
+        esco = esco.loc[:,["o&m", 'energy',    "infra"    , "maintenance passive preventive", "gardes de securite","discount", "volume discount", "opex_without_discount"]]
+        ratio = esco.loc[: ,["o&m", 'energy',    "infra"    , "maintenance passive preventive", "gardes de securite","discount"]]/esco["opex_without_discount"].values[0]
+        #ratio["volume discount"] = esco["volume discount"].divide(esco["opex_without_discount"].values[0])
+        data_final["discount"] = 0
+        data_final["volume discount"] = 0    
+        data_final["o&m"] = ratio["o&m"].values * data_final["month_total"]
+        data_final["energy"] = ratio["energy"].values * data_final["month_total"]
+        data_final["infra"] = ratio["infra"].values * data_final["month_total"]
+        data_final["maintenance passive préventive"] = ratio["maintenance passive préventive"].values * data_final["month_total"]
+        data_final["gardes de sécurité"] = ratio["gardes de sécurité"].values * data_final["month_total"]
+        logging.info("save to minio")
+        save_minio(client, objet["bucket"], f'{objet["folder"]}-cleaned', date, data_final)
 
 
 
