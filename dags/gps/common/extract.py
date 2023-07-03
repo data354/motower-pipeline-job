5b6051a8a6925a8a8e44e90186e3d69b5c7030df
Yasmine Stéphanie KOUADIO <yasminekouadio@hotmail.fr>
2023-07-03 13:27:14 +0000

    test sensors

--- dags/gps/common/extract.py
+++ dags/gps/common/extract.py
@@ -126,24 +126,13 @@ def file_exists(hostname: str, user: str, password: str, date: str, smtp_host, s
     filename = f"extract_vbm_{date.replace('-', '')}.csv"
     logging.info(f"Reading {filename} ")
     try:
-        print(hostname)
-        print(user)
-        print(password)
-        print(date)
         server = ftplib.FTP(hostname, user, password, timeout=200)
     except Exception as error :
-        content  = f"Authentication error to FTP server"
-        subject=  f"Authentication error."
-        send_email(host= smtp_host, port= smtp_port, user = smtp_user, receivers = receivers, subject = subject, content=content)
-        return False
+        raise ConnectionError("Connection to FTP server failed.") from error
     logging.info("check file")
     server.cwd(CONFIG["ftp_dir"])
     file_list = server.nlst()
-    print(file_list[0:10])
-    if filename not in file_list:
-        content  = f"Missing file {filename}. Please can you upload the file as soon as possible?"
-        subject=  f"Missing file {filename}."
-        send_email(host= smtp_host, port= smtp_port, user = smtp_user, receivers = receivers, subject = subject, content=content)
+    print(filename in file_list)
     return filename in file_list
 
 def extract_ftp(hostname: str, user: str, password: str, date: str) -> pd.DataFrame:
