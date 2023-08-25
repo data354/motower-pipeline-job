2225117cd46ce76eb3f22312bc1689e7f2ec0546
Yasmine Stéphanie KOUADIO <yasminekouadio@hotmail.fr>
2023-08-25 16:24:19 +0000

    add daily dag

--- dags/gps/common/rwpg.py
+++ dags/gps/common/rwpg.py
@@ -191,6 +191,11 @@ def write_pg(host: str, database:str, user: str, password: str,
         
 
         cur.execute(create_query)
+    if table == "motower_daily":
+        delete_query = f"DELETE FROM {table} WHERE jour = '{data.jour.unique()[0]}'"
+    if table == "motower_monthly":
+        #delete_query = "DELETE FROM "+table+ " WHERE mois = %s;", (data.mois.unique()[0],)
+        delete_query = f"DELETE FROM {table} WHERE mois = '{data.mois.unique()[0]}'"
     cur.execute(delete_query)
     conn.commit()
     cur.close()
