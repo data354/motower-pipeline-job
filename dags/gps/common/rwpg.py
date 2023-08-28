92c39958a66204d1c9febdea6cb71334f7333439
Yasmine Stéphanie KOUADIO <yasminekouadio@hotmail.fr>
2023-08-28 16:52:51 +0000

    change weekly logique

--- dags/gps/common/rwpg.py
+++ dags/gps/common/rwpg.py
@@ -275,7 +275,7 @@ def write_pg(host: str, database:str, user: str, password: str,
     if table == "motower_monthly":
         #delete_query = "DELETE FROM "+table+ " WHERE mois = %s;", (data.mois.unique()[0],)
         delete_query = f"DELETE FROM {table} WHERE mois = '{data.mois.unique()[0]}'"
-    if table == "congestion":
+    if table == "motower_daily":
         delete_query = f"DELETE FROM {table} WHERE jour = '{data.jour.unique()[0]}'"
     cur.execute(delete_query)
     conn.commit()
