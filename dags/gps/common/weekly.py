f3d71835d4c9169d1159fb3e6b5d586ebd042f4c
Yasmine Stéphanie KOUADIO <yasminekouadio@hotmail.fr>
2023-08-28 16:56:53 +0000

    change weekly logique

--- dags/gps/common/weekly.py
+++ dags/gps/common/weekly.py
@@ -86,6 +86,7 @@ def motower_weekly(client, endpoint: str, accesskey: str, secretkey: str, thedat
     congestion["id_site"] = congestion["id_site"].astype("str")
     daily["code_oci"] = daily["code_oci"].astype("str")
     weekly = daily.merge(congestion, left_on =["code_oci"], right_on = ["id_site"], how="left")
+    print(weekly.columns)
     #weekly = weekly.drop(columns=["jour.y"])
 
 
