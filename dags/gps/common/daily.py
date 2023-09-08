32bff060a89240d79b0c99a9f9798b3465c163f0
Yasmine Stéphanie KOUADIO <yasminekouadio@hotmail.fr>
2023-09-08 12:20:18 +0000

    delete trafic to daily and add cssr, alarm, etc

--- dags/gps/common/daily.py
+++ dags/gps/common/daily.py
@@ -20,59 +20,6 @@ def compute_segment(ca:float, loc:str)->str:
         segment = "PREMIUM" if ca>=10000000 else "NORMAL" if ca>=4000000 else "A DEVELOPER"
     return segment
 
-
-
-def cleaning_daily_trafic(client, endpoint: str, accesskey: str, secretkey: str, date: str):
-    """
-    Cleans traffic files
-    Args:
-        - client: Minio client
-        - endpoint: Minio endpoint
-        - accesskey: Minio access key
-        - secretkey: Minio secret key
-        - date: execution date (provided by airflow %Y-%m-%d)
-    Return:
-        None
-    """
-     # Find the required object in the CONFIG dictionary
-    objet = next((table for table in CONFIG["tables"] if table["name"] == "ks_daily_tdb_radio_drsi"), None)
-    if not objet:
-        raise ValueError("Table ks_daily_tdb_radio_drsi not found.")
-     # Check if bucket exists
-    if not client.bucket_exists(objet["bucket"]):
-        raise ValueError(f"Bucket {objet['bucket']} does not exist.")
-     # Split the date into parts
-    date_parts = date.split("-")
-    filename = get_latest_file(client, objet["bucket"], prefix = f"{objet['folder']}/{date_parts[0]}/{date_parts[1]}/{date_parts[2]}")
-    try:
-        logging.info("read %s", filename)
-        trafic = pd.read_csv(f"s3://{objet['bucket']}/{filename}",
-                                storage_options={
-                                "key": accesskey,
-                                "secret": secretkey,
-                "client_kwargs": {"endpoint_url": f"http://{endpoint}"}
-                }
-                    )
-    except Exception as error:
-        raise OSError(f"{filename} don't exists in bucket") from error
-    trafic.columns = trafic.columns.str.lower()
-    trafic = trafic.loc[trafic["techno"].isin(["2G", "3G", "4G"]), :]
-    try:
-        trafic["trafic_data_go"] = trafic["trafic_data_go"].astype("float")
-        trafic["trafic_voix_erl"] = trafic["trafic_voix_erl"].astype("float")
-    except AttributeError :
-        trafic["trafic_data_go"] = trafic["trafic_data_go"].str.replace(",", ".").astype("float")
-        trafic["trafic_voix_erl"] = trafic["trafic_voix_erl"].str.replace(",", ".").astype("float")
-
-    trafic = trafic[["date_id", "id_site", "trafic_data_go", "trafic_voix_erl", "techno" ]]
-    trafic = trafic.groupby(["date_id", "id_site", "techno"]).sum()
-    trafic = trafic.unstack()
-    trafic.columns = ["_".join(d) for d in trafic.columns]
-    trafic.reset_index(drop=False, inplace=True)
-    trafic.columns = ["jour", "id_site", "trafic_data_2g", "trafic_data_3g", "trafic_data_4g", "trafic_voix_2g", "trafic_voix_3g", "trafic_voix_4g" ]
-     # Save the cleaned dataFrame to Minio
-    save_minio(client, objet["bucket"], date, trafic, f'{objet["folder"]}-cleaned')
-    
     
     
     
@@ -120,42 +67,26 @@ def motower_daily(client, endpoint: str, accesskey: str, secretkey: str, date: s
         raise ValueError(f"{filename} does not exist in bucket.") from error
 
 
-    logging.info("get trafic")
-    objet = next((table for table in CONFIG["tables"] if table["name"] == "ks_daily_tdb_radio_drsi"), None)
-    if objet is None:
-        raise ValueError("Table 'ks_daily_tdb_radio_drsi' not found in configuration")
-    filename = get_latest_file(client, objet["bucket"], prefix = f"{objet['folder']}-cleaned/{date_parts[0]}/{date_parts[1]}/{date_parts[2]}")
-    try:
-        logging.info("read %s", filename)
-        trafic = pd.read_csv(f"s3://{objet['bucket']}/{filename}",
-                                storage_options={
-                                "key": accesskey,
-                                "secret": secretkey,
-                "client_kwargs": {"endpoint_url": f"http://{endpoint}"}
-                }
-                    )
-    except Exception as error:
-        raise OSError(f"{filename} don't exists in bucket") from error
-
     logging.info("merge bdd and CA")
     bdd_ca = bdd.merge(ca, left_on=["code oci id"], right_on = ["id_site" ], how="left")
-    logging.info("add trafic")
-    bdd_ca_trafic = bdd_ca.merge(trafic, left_on=["code oci id"], right_on = ["id_site" ], how="left")
+   
     logging.info("prepare final daily data")
     
-    bdd_ca_trafic["jour"] = exec_date
-    df_final = bdd_ca_trafic.loc[:,["jour", "code oci", "code oci id", "autre code", "clutter", "commune", "departement", "type du site", "type geolocalite", "gestionnaire",
+    bdd_ca["jour"] = exec_date
+    df_final = bdd_ca.loc[:,["jour", "code oci", "code oci id", "autre code", "clutter", "commune", "departement", "type du site", "type geolocalite", "gestionnaire",
                            "latitude", "longitude", "localisation", "partenaires", "proprietaire", "position site", "site", "statut", "projet", "region",
                            "ca_data", "ca_voix", "ca_total", "parc", "parc_data", "parc_2g", "parc_3g", "parc_4g", "parc_5g", "parc_other", "trafic_data_in",
-                           "trafic_voix_in", "trafic_data_2g", "trafic_data_3g", "trafic_data_4g", "trafic_voix_2g", "trafic_voix_3g", "trafic_voix_4g"]]
+                           "trafic_voix_in"]]
     
     df_final.columns = ["jour", "code_oci","code_oci_id", "autre_code", "clutter", "commune", "departement", "type_du_site", "type_geolocalite", "gestionnaire",
                            "latitude", "longitude", "localisation", "partenaires", "proprietaire", "position_site", "site", "statut", "projet", "region",
                            "ca_data", "ca_voix", "ca_total", "parc_global", "parc_data", "parc_2g", "parc_3g", "parc_4g", "parc_5g", "autre_parc", "trafic_data_in",
-                           "trafic_voix_in", "trafic_data_2g", "trafic_data_3g", "trafic_data_4g", "trafic_voix_2g", "trafic_voix_3g", "trafic_voix_4g"]
+                           "trafic_voix_in"]
     
-    df_final["trafic_data_total"] = df_final["trafic_data_2g"] + df_final["trafic_data_3g"] + df_final["trafic_data_4g"]
-    df_final["trafic_voix_total"] = df_final["trafic_voix_2g"] + df_final["trafic_voix_3g"] + df_final["trafic_voix_4g"]
+    df_final["trafic_data_in_mo"] = df_final["trafic_data_in_mo"]  / 1000
+    
+    # df_final["trafic_data_total"] = df_final["trafic_data_2g"] + df_final["trafic_data_3g"] + df_final["trafic_data_4g"]
+    # df_final["trafic_voix_total"] = df_final["trafic_voix_2g"] + df_final["trafic_voix_3g"] + df_final["trafic_voix_4g"]
 
     # INIT COLUMNS
     df_final["ca_norm"] = None
--- dags/gps/custom_dags/daily.py
+++ dags/gps/custom_dags/daily.py
@@ -6,10 +6,10 @@ from airflow.sensors.python import PythonSensor
 from airflow.models import Variable
 from airflow import DAG
 from gps import CONFIG
-from gps.common.extract import extract_ftp, list_ftp_file
+from gps.common.extract import extract_ftp, list_ftp_file, extract_pg
 from gps.common.rwminio import save_minio
 from gps.common.alerting import send_email, alert_failure, get_receivers
-from gps.common.daily import motower_daily, cleaning_daily_trafic
+from gps.common.daily import motower_daily
 from gps.common.rwpg import write_pg
 from gps.custom_dags.weekly import extract_v2
 
@@ -24,7 +24,7 @@ PG_SAVE_DB = Variable.get('pg_save_db')
 PG_SAVE_USER = Variable.get('pg_save_user')
 PG_SAVE_PASSWORD = Variable.get('pg_save_password')
 
-DATE = "{{ macros.ds_add(ds, -1) }}"
+INGEST_DATE = "{{ macros.ds_add(ds, -1) }}"
 
 MINIO_ENDPOINT = Variable.get('minio_host')
 MINIO_ACCESS_KEY = Variable.get('minio_access_key')
@@ -34,27 +34,45 @@ SMTP_HOST = Variable.get('smtp_host')
 SMTP_PORT = Variable.get('smtp_port')
 SMTP_USER = Variable.get('smtp_user')
 
+PG_HOST = Variable.get('pg_host')
+PG_DB = Variable.get('pg_db')
+PG_USER = Variable.get('pg_user')
+PG_PASSWORD = Variable.get('pg_password')
+
 CLIENT = Minio( MINIO_ENDPOINT,
         access_key= MINIO_ACCESS_KEY,
         secret_key= MINIO_SECRET_KEY,
         secure=False)
 
 
+
+
+def extract_job(**kwargs):
+    data = extract_pg(host = PG_HOST, database= PG_DB, user= PG_USER,
+            password= PG_PASSWORD , table= kwargs["thetable"] , date= kwargs["ingest_date"])
+    if kwargs["thetable"] == "hourly_datas_radio_prod" and data.empty:
+        data = extract_pg(host = PG_HOST, database= PG_DB, user= PG_USER,
+            password= PG_PASSWORD , table = "hourly_datas_radio_prod_archive" , date= kwargs["ingest_date"])
+    if  data.empty:
+        raise RuntimeError(f"No data for {kwargs['ingest_date']}")
+    
+    save_minio(CLIENT, kwargs["bucket"] , kwargs["ingest_date"], data, kwargs["folder"])
+
 def extract_ftp_job(**kwargs):
     """
     extract ftp files callable
  
     """
-    data = extract_ftp(FTP_HOST, FTP_USER, FTP_PASSWORD, kwargs["date"])
+    data = extract_ftp(FTP_HOST, FTP_USER, FTP_PASSWORD, kwargs["ingest_date"])
     if  data.empty:
-        raise RuntimeError(f"No data for {kwargs['date']}")
-    save_minio(CLIENT, kwargs["bucket"], kwargs["date"], data, kwargs["folder"])
+        raise RuntimeError(f"No data for {kwargs['ingest_date']}")
+    save_minio(CLIENT, kwargs["bucket"], kwargs["ingest_date"], data, kwargs["folder"])
 
 def check_file(**kwargs):
     """
         check if file exists
     """
-    filename = f"extract_vbm_{kwargs['date'].replace('-', '')}.csv"
+    filename = f"extract_vbm_{kwargs['ingest_date'].replace('-', '')}.csv"
     liste = list_ftp_file(FTP_HOST, FTP_USER, FTP_PASSWORD)
     if filename in liste:
         return True
@@ -65,7 +83,7 @@ def send_email_onfailure(**kwargs):
     """
     send email if sensor failed
     """
-    date_parts = kwargs["date"].split("-")
+    date_parts = kwargs["ingest_date"].split("-")
     filename = f"{kwargs['code']}_{date_parts[0]}{date_parts[1]}.xlsx"
     subject = f"  Missing file {filename}"
     content = f"Missing file {filename}. please provide file asap"
@@ -79,14 +97,14 @@ def gen_motower_daily(**kwargs):
         MINIO_ENDPOINT,
         MINIO_ACCESS_KEY,
         MINIO_SECRET_KEY,
-        kwargs["date"],
+        kwargs["ingest_date"],
         PG_SAVE_HOST, 
         PG_SAVE_USER, 
         PG_SAVE_PASSWORD, PG_SAVE_DB)
     if not data.empty:
         write_pg(PG_SAVE_HOST, PG_SAVE_DB, PG_SAVE_USER, PG_SAVE_PASSWORD, data, "motower_daily")
     else:
-        raise RuntimeError(f"No data for {kwargs['date']}")
+        raise RuntimeError(f"No data for {kwargs['ingest_date']}")
 
 def on_failure(context):
     """
@@ -134,17 +152,10 @@ with DAG(
         retries=0,
         timeout=10,
         python_callable= check_file,
-        on_failure_callback = partial(send_email_onfailure, {'date': DATE, 'host': SMTP_HOST,
-            'port':SMTP_PORT,
-            'users': SMTP_USER,
-            'code': 'CA_SITES' }),
+        on_failure_callback = on_failure,
         op_kwargs={
       
-              'date': DATE,
-        #     'smtp_host': SMTP_HOST,
-        #     'smtp_user': SMTP_USER,
-        #     'smtp_port': SMTP_PORT,
-        #     'receivers': CONFIG["airflow_receivers"]
+              'date': INGEST_DATE,
         },
 
     )
@@ -153,73 +164,98 @@ with DAG(
         python_callable=send_email_onfailure,
         trigger_rule='one_failed',  # Exécuter la tâche si le sensor échoue
         op_kwargs={
-            'date': DATE,
+            'date': INGEST_DATE,
             'host': SMTP_HOST, 
             'port':SMTP_PORT,
             'users': SMTP_USER,
-            'code': ' '  # renvoie le mail de JEAN LOUIS
+            'code': 'CA_SITES'  
         }
     )
-    table_config = next((table for table in CONFIG["tables"] if table["name"] == "caparc"), None)
-    get_caparc = PythonOperator(
-                task_id= "get_caparc",
+
+    tasks = []
+    for table_config in CONFIG["tables"]:
+        if table_config["name"] in ["faitalarme", "hourly_datas_radio_prod",  "Taux_succes_2g", "Taux_succes_3g"]:
+            task_id = f'ingest_{table_config["name"]}'
+            callable_fn = extract_job 
+            task = PythonOperator(
+                task_id=task_id,
                 provide_context=True,
-                python_callable=extract_ftp_job,
-                on_failure_callback=on_failure,
+                python_callable=callable_fn,
                 op_kwargs={
                     'thetable': table_config["name"],
                     'bucket': table_config["bucket"],
                     'folder': table_config["folder"],
                     'table': table_config["table"],
-                    'date': DATE
+                    'ingest_date': INGEST_DATE
                 },
                 dag=dag,
             )
-    table_config = next((table for table in CONFIG["tables"] if table["name"] == "ks_daily_tdb_radio_drsi"), None)
-    extract_trafic = PythonOperator(
-                task_id="extract_trafic",
+            tasks.append(task)
+        if table_config["name"] == "caparc":
+            task_id = f'ingest_{table_config["name"]}'
+            callable_fn =  extract_ftp_job
+            ingest_caparc = PythonOperator(
+                task_id=task_id,
                 provide_context=True,
-                python_callable=extract_v2,
-                on_failure_callback=on_failure,
+                python_callable=callable_fn,
                 op_kwargs={
                     'thetable': table_config["name"],
                     'bucket': table_config["bucket"],
                     'folder': table_config["folder"],
                     'table': table_config["table"],
-                    'date': DATE
+                    'ingest_date': INGEST_DATE
                 },
                 dag=dag,
             )
-    clean_trafic_task = PythonOperator(
-            task_id="clean_trafic_task",
-            provide_context=True,
-            python_callable=cleaning_daily_trafic,
-            on_failure_callback=on_failure,
-            op_kwargs={
-                "client": CLIENT,
-                "endpoint": MINIO_ENDPOINT,
-                "accesskey": MINIO_ACCESS_KEY,
-                "secretkey": MINIO_SECRET_KEY,
-                "date": DATE,
-            },
-            # on_failure_callback=on_failure,
-            dag=dag,
-        )
-    
+
     motower_task = PythonOperator(
             task_id="motower_task",
             provide_context=True,
             python_callable=gen_motower_daily,
             on_failure_callback=on_failure,
             op_kwargs={
-                "date": DATE
+                "date": INGEST_DATE
             },
             dag=dag,
         )
-    [check_file_sensor >> send_email_task , extract_trafic >> clean_trafic_task]
-    [check_file_sensor >> get_caparc , extract_trafic>> clean_trafic_task] >> motower_task
+    
+    [check_file_sensor >> send_email_task  ]>>motower_task
+    tasks
 
 
+    #table_config = next((table for table in CONFIG["tables"] if table["name"] == "ks_daily_tdb_radio_drsi"), None)
+    # extract_trafic = PythonOperator(
+    #             task_id="extract_trafic",
+    #             provide_context=True,
+    #             python_callable=extract_v2,
+    #             on_failure_callback=on_failure,
+    #             op_kwargs={
+    #                 'thetable': table_config["name"],
+    #                 'bucket': table_config["bucket"],
+    #                 'folder': table_config["folder"],
+    #                 'table': table_config["table"],
+    #                 'date': DATE
+    #             },
+    #             dag=dag,
+    #         )
+    # clean_trafic_task = PythonOperator(
+    #         task_id="clean_trafic_task",
+    #         provide_context=True,
+    #         python_callable=cleaning_daily_trafic,
+    #         on_failure_callback=on_failure,
+    #         op_kwargs={
+    #             "client": CLIENT,
+    #             "endpoint": MINIO_ENDPOINT,
+    #             "accesskey": MINIO_ACCESS_KEY,
+    #             "secretkey": MINIO_SECRET_KEY,
+    #             "date": DATE,
+    #         },
+    #         # on_failure_callback=on_failure,
+    #         dag=dag,
+    #     )
+    
+    
+    
 
 
 
