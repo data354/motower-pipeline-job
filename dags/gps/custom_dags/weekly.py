372b2159626f1274bcc3c20ec0981b020d317a64
Yasmine Stéphanie KOUADIO <yasminekouadio@hotmail.fr>
2023-09-11 14:21:26 +0000

    delete congestion

--- dags/gps/custom_dags/weekly.py
+++ dags/gps/custom_dags/weekly.py
@@ -133,7 +133,7 @@ with DAG(
                 "endpoint": MINIO_ENDPOINT,
                 "accesskey": MINIO_ACCESS_KEY,
                 "secretkey": MINIO_SECRET_KEY,
-                "ingest_date": DATE,
+                "date": DATE,
             },
             # on_failure_callback=on_failure,
             dag=dag,
