0120ad6e83e508650888054c8ba508ac228d23d0
Yasmine Stéphanie KOUADIO <yasminekouadio@hotmail.fr>
2023-09-12 01:25:01 +0000

    weekly

--- dags/gps/common/rwpg.py
+++ dags/gps/common/rwpg.py
@@ -260,7 +260,7 @@ def write_pg(host: str, database:str, user: str, password: str,
     if table in ["motower_daily"]:
         delete_query = f"DELETE FROM {table} WHERE jour = '{data.jour.unique()[0]}'"
     if table == "motower_weekly":
-        exec_date = data.jour.unique()[0]
+        exec_date = data["jour"].astype(dt).unique()[0]
         delete_query = f"DELETE FROM {table} WHERE EXTRACT(WEEK FROM jour) = {exec_date.isocalendar()[1]} and EXTRACT(YEAR FROM jour) = {exec_date.isocalendar()[0]} "
     if table == "motower_monthly":
         #delete_query = "DELETE FROM "+table+ " WHERE mois = %s;", (data.mois.unique()[0],)
