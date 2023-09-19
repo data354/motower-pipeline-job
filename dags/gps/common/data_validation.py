73a1ab02b36fe29ddb080eeabd90d8439cd2f9f9
Yasmine Stéphanie KOUADIO <yasminekouadio@hotmail.fr>
2023-09-19 12:39:26 +0000

    new daily ingestion with validation : ca+bdd

--- dags/gps/common/data_validation.py
+++ dags/gps/common/data_validation.py
@@ -7,7 +7,7 @@ def validate_column(df, col:str, date = None): # just for monthly
         validate data 
     """
     logging.info("START VALIDATION OF COLUMN %s", col)
-    thresolds = next((d[col] for d in CONFIG["thresold"]), None)
+    thresolds = next((CONFIG["thresold"][col]), None)
     df_not_valid = df[~((df[col] >= thresolds["min"]) & (df[col] <= thresolds["max"]) )]
     if df_not_valid.shape[0]:
         message = f"These dates have invalid {col}: {df_not_valid.loc[:, ['day_id', col]].to_string()}" if date is None else f" CA PARC FILE OF {date} have invalid {col}: {df_not_valid.loc[:, ['day_id', col]].to_string()}"
@@ -19,7 +19,7 @@ def validate_site_actifs(df_bdd_site):
         validate number of actifs sites from bdd site
     """
     logging.info("START VALIDATION ")
-    thresolds = next((d["sites_actifs"] for d in CONFIG["thresold"]), None)
+    thresolds = next((CONFIG["thresold"]["sites_actifs"]), None)
     number = len(df_bdd_site["code_oci"].unique())
     mois = df_bdd_site["mois"].unique()[0]
     if not((number >= thresolds["min"]) & (number <= thresolds["max"])):
