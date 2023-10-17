6400a066cc14b7733918ce014f11ec61c4efb46b
Yasmine Stéphanie KOUADIO <yasminekouadio@hotmail.fr>
2023-10-17 12:53:46 +0000

    add segment and previous segment

--- dags/gps/common/motower_daily.py
+++ dags/gps/common/motower_daily.py
@@ -1,4 +1,5 @@
 import logging
+import math
 import pandas as pd
 import psycopg2
 from datetime import datetime
@@ -10,19 +11,16 @@ from gps.common.data_validation import validate_column, validate_site_actifs
 
 
 
-def compute_segment(ca:float, loc:str):
+def compute_segment(ca:float, loc:str)->str:
     segment = None
     if not ca or not loc :
         return segment
-    if loc.isinstance(str):
-        loc = loc.lower()
-        if loc=='abidjan':
-            segment = "PREMIUM" if ca>=20000000 else "NORMAL" if ca>=10000000 else "A DEVELOPER"
-        if loc=='intérieur':
-            segment = "PREMIUM" if ca>=10000000 else "NORMAL" if ca>=4000000 else "A DEVELOPER"
-        return segment
-    else:
-        return None
+    loc = loc.lower()
+    if loc=='abidjan':
+        segment = "PREMIUM" if ca>=20000000 else "NORMAL" if ca>=10000000 else "A DEVELOPER"
+    if loc=='intérieur':
+        segment = "PREMIUM" if ca>=10000000 else "NORMAL" if ca>=4000000 else "A DEVELOPER"
+    return segment
 
 
     
@@ -140,7 +138,8 @@ def generate_daily_caparc(client, endpoint: str, accesskey: str, secretkey: str,
                 mtd_rows = month_data.loc[month_data["code_oci"] == code_oci, :]
                 ca_mtd = mtd_rows["ca_total"].sum()
                 ca_norm = ca_mtd * 30 / date_row.day
-                print(loc_row)                
+                print(loc_row)         
+                print(math.isnan(loc_row))       
                 segment = compute_segment(ca_norm, loc_row)
                 df_final.loc[idx, ["ca_mtd", "ca_norm", "segment"]] = [ca_mtd, ca_norm, segment]
 
